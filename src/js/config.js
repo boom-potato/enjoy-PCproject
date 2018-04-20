8160641b921f2984a0169990f5d3c613c0a36642
xiaoyang <yangyu.92@qq.com>
2018-04-20 18:52:55 +0800

    主页的初步完成

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,7 @@ require.config({
 		jquery : "lib/jquery/jquery-1.12.4.min",
 		cookie : "lib/jquery_plugins/jquery.cookie",
 		zoom : "lib/jquery_plugins/jquery.elevateZoom-3.0.8.min",
-		boot : "lib/bootstrap/js/bootstrap.min",
+		boot : "lib/bootstrap/js/bootstrap",
 		template : "lib/artTemplate/template",
 		load : "js/loadHeaderFooter"
 	},
@@ -14,6 +14,9 @@ require.config({
 		},
 		zoom : {
 			deps : ["jquery"]
-		}
+		},
+		boot : {
+			deps : ["jquery"]
+		},
 	}
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 require(["config"], function(){
-	require(["jquery", "load", "boot"], function($){
+	require(["jquery", "template", "load", "boot"], function($, template){
 
         /* carousel */
         // $.getJSON("/mock/carousel.json", function(data){
@@ -7,12 +7,82 @@ require(["config"], function(){
         //     let html = template("carousel_temp", data);
         //     $(".banner_inner").prepend(html);
         // })
+        /* 加载推荐商品 */ 
+		$.getJSON("/mock/popular.json", function(data){
+			data = {popular : data.res_body.data};
+			let html = template("popular_tem", data);
+			$(".popular_list").html(html);
+		});
 
-		// $.getJSON("/mock/list.json", function(data){
-		// 	data = {list : data.res_body.data};
-		// 	let html = template("list_template", data);
-		// 	$(".list").html(html);
-		// });
+        /*  第一层 加载健康生活商品 */ 
+        $.getJSON("/mock/index_prodRight.1.json", function(data){
+            data = {health : data.res_body.data};
+            let html = template("health_temp", data);
+            $(".bannerRightList").html(html);
+        });
+
+        $.getJSON("/mock/index_NavLeft.1.json" ,function(data){
+            data = {healthNav : data.res_body.data};
+            // console.log(data);
+            let html = template("healthNav_temp", data);
+            $(".themes").html(html);
+        })
+
+        /*  第二层 加载健康健康食品 */ 
+        $.getJSON("/mock/index_prodRight.2.json", function(data){
+            data = {health : data.res_body.data};
+            let html = template("health_temp", data);
+            $("#bannerRightList2").html(html);
+        });
+
+        $.getJSON("/mock/index_NavLeft.2.json" ,function(data){
+            data = {healthNav : data.res_body.data};
+            // console.log(data);
+            let html = template("healthNav_temp", data);
+            $("#themes2").html(html);
+        })
+
+        /*  第三层 加载母婴用品 */ 
+       $.getJSON("/mock/index_prodRight.3.json", function(data){
+            data = {health : data.res_body.data};
+            let html = template("health_temp", data);
+            $("#bannerRightList3").html(html);
+        });
+
+        $.getJSON("/mock/index_NavLeft.3.json" ,function(data){
+            data = {healthNav : data.res_body.data};
+            // console.log(data);
+            let html = template("healthNav_temp", data);
+            $("#themes3").html(html);
+        });
+
+        /*  第四层 加载时尚臻藏 */ 
+        $.getJSON("/mock/index_prodRight.4.json", function(data){
+            data = {health : data.res_body.data};
+            let html = template("health_temp", data);
+            $("#bannerRightList4").html(html);
+        });
+
+        $.getJSON("/mock/index_NavLeft.4.json" ,function(data){
+            data = {healthNav : data.res_body.data};
+            // console.log(data);
+            let html = template("healthNav_temp", data);
+            $("#themes4").html(html);
+        });
+
+        /*  第五层 加载时尚科技 */ 
+        $.getJSON("/mock/index_prodRight.5.json", function(data){
+            data = {health : data.res_body.data};
+            let html = template("health_temp", data);
+            $("#bannerRightList5").html(html);
+        });
+
+        $.getJSON("/mock/index_NavLeft.5.json" ,function(data){
+            data = {healthNav : data.res_body.data};
+            // console.log(data);
+            let html = template("healthNav_temp", data);
+            $("#themes5").html(html);
+        });
 
 	});
 });
